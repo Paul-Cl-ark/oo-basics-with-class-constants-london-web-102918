efe774dc21f7f33a91a79b684e2703bd8cc35125
Paul-Cl-ark <clark.paul123@gmail.com>
2018-10-31 18:24:10 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,16 @@
+require 'pry'
+
 class Shoe
+
+  BRANDS = Array.new
+
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
   def initialize(brand)
     @brand = brand
+    unless BRANDS.include? self.brand then BRANDS.push(brand)
+    end
   end
 
   def cobble
